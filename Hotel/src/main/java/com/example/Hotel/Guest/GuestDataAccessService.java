444bb7fe5d287846d7dd665e93c179f9236f7302
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2020-09-17 01:50:32 +0200

    Working database need add logic for it

--- Hotel/src/main/java/com/example/Hotel/Guest/GuestDataAccessService.java
+++ Hotel/src/main/java/com/example/Hotel/Guest/GuestDataAccessService.java
@@ -1,6 +1,8 @@
 package com.example.Hotel.Guest;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,14 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class GuestDataAccessService implements GuestDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public GuestDataAccessService(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertGuest(Guest guest) {
         return 0;
@@ -16,12 +26,33 @@ public class GuestDataAccessService implements GuestDao{
 
     @Override
     public List<Guest> selectAllGuests() {
-        return List.of(new Guest("12334","From POSTGRES DB","From postgres dB",101));
+        final String sql = "SELECT IdCardNr,first_name,surname,room  FROM guest"; // as well i could use *
+        List<Guest> guests =jdbcTemplate.query(sql,(resultSet,i)->{
+            return new Guest(resultSet.getString("IdCardNr"),
+                             resultSet.getString("first_name"),
+                             resultSet.getString("surname"),
+                             resultSet.getInt("room"));
+        });
+        return guests;
     }
 
     @Override
     public Optional<Guest> selectGuestById(String idCardNr) {
-        return Optional.empty();
+        final String sql = "SELECT IdCardNr,first_name,surname,room  FROM guest WHERE IdCardNr = ?";
+
+        Guest guest = jdbcTemplate.queryForObject(
+            sql,
+            new Object[]{idCardNr},
+                (resultSet, i) -> {
+                String GuestIdCardNr = resultSet.getString(("IdCardNr"));
+                String name = resultSet.getString("first_name");
+                String surname = resultSet.getString("surname");
+                Integer room = resultSet.getInt("room");
+                return new Guest(GuestIdCardNr,name,surname,room);
+         });
+        return Optional.ofNullable(guest);
+
+
     }
 
     @Override
